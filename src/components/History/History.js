0ad7846051b711a4abad4f37a43d79fe1b274aeb
Syeda Rida Fatima <syedaridafatima67@gmail.com>
2023-08-23 01:28:55 +0500

    dummy data removed

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-// import Records from './Records';
+import { ToastContainer } from 'react-toastify';
 import { getSurveyData } from '../Questionnaire/Survey';
 
 import Links from '../Links';
@@ -9,49 +8,16 @@ import Links from '../Links';
 function History() {
 
 
-    // const data = getSurveyData();
-    const data = {
-        "id": 12,
-        "gender": 0,
-        "income_group": 0,
-        "sensing": 0.5714285714285714,
-        "introvert": 0.5714285714285714,
-        "Judging": 0.42857142857142855,
-        "Thinking": 0.7142857142857143,
-        "logical_intelligence": 2.8,
-        "Nature_intelligence": 1.2,
-        "Visual_intelligence": 2.8,
-        "Musical_intelligence": 2.6,
-        "Body_intelligence": 2.4,
-        "Interpersonal_intelligence": 2.8,
-        "Intrapersonal_intelligence": 3.8,
-        "Verbal_intelligence": 3.2,
-        "Existential_intelligence": 2.6,
-        "Engineering_Field1": "Computer and Information Systems Engineering",
-        "Engineering_Field2": "Medical Technology",
-        "Engineering_Field3": "Software Engineering",
-        "Engineering_Field4": "Civil Engineering",
-        "Engineering_Field5": "MBBS",
-        "Medical_Field1": "Mechanical Engineering",
-        "Medical_Field2": "Electrical Engineering",
-        "Medical_Field3": "Nutrition Sciences",
-        "button": 2.0,
-        "user": 6
-    }
+    const data = getSurveyData();
+
     const intelligenceData = data ? [
 
         data.sensing, data.introvert, data.Judging, data.Thinking
-        // data.logical_intelligence, data.Nature_intelligence, data.Visual_intelligence,
-        // data.Musical_intelligence, data.Body_intelligence, data.Interpersonal_intelligence,
-        // data.Intrapersonal_intelligence, data.Verbal_intelligence, data.Existential_intelligence
 
     ] : [];
-    //  const totalIntelligence = intelligenceData.reduce((sum, intelligence) => sum + intelligence, 0);
     const intelligenceDataLabels = [
         'Intuitive', 'Introvert', 'Perceiving', 'Feeling'
-        // 'Logical Intelligence', 'Nature Intelligence', 'Visual Intelligence',
-        // 'Musical Intelligence', 'Body Intelligence', 'Interpersonal Intelligence',
-        // 'Intrapersonal Intelligence', 'Verbal Intelligence', 'Existential Intelligence'
+
     ];
     const [OpenRecords, setOpenRecords] = useState(true);
     // Extracting engineering and medical fields from the data object
@@ -179,85 +145,3 @@ function History() {
 export { History };
 
 
-
-
-// import React from 'react';
-
-// function History() {
-//     // Example data for the bars
-//     const barsData = [
-//         { label: 'Bar 1', percentage: 88 },
-//         { label: 'Bar 2', percentage: 60 },
-//         { label: 'Bar 3', percentage: 40 },
-//         { label: 'Bar 4', percentage: 20 },
-//         { label: 'Bar 5', percentage: 10 },
-//     ];
-
-//     return (
-
-//         <div className="container mx-auto p-4 mt-40">
-//             <h1 className="text-4xl font-bold mb-12">AI BASED CAREER MENTOR</h1>
-//             <h2 className="text-2xl font-bold mb-8">Hello, I'm result</h2>
-//             <div>
-//                 {barsData.map((bar, index) => (
-//                     <div key={index} className="mb-4">
-//                         <div className="flex items-center">
-//                             <div className="w-24">{bar.label}</div>
-//                             <div className="bg-gray-200 h-4 flex-grow ml-4">
-//                                 <div
-//                                     className="bg-blue-500 h-full"
-//                                     style={{ width: `${bar.percentage}%` }}
-//                                 ></div>
-//                             </div>
-//                             <div className="ml-4">{bar.percentage}%</div>
-//                         </div>
-//                     </div>
-//                 ))}
-//             </div>
-//             {/* <div className="container mx-auto p-4 mt-40">
-//                 <h1 className="text-4xl font-bold mb-12">AI BASED CAREER MENTOR</h1>
-//                 <h2 className="text-2xl font-bold mb-8">Combined History Snapshot</h2>
-//                 <div>
-//                     {barsData.map((bar, index) => (
-//                         <div key={index} className="mb-4">
-//                             <div className="flex items-center">
-//                                 <div className="w-24">{bar.label}</div>
-//                                 <div className="bg-gray-200 h-4 flex-grow ml-4">
-//                                     <div
-//                                         className="bg-blue-500 h-full"
-//                                         style={{ width: `${bar.percentage}%` }}
-//                                     ></div>
-//                                 </div>
-//                                 <div className="ml-4">{bar.percentage}%</div>
-//                             </div>
-//                         </div>
-//                     ))}
-//                 </div>
-//                 <div className="container mx-auto p-4 mt-300">
-//                     <h1 className="text-4xl font-bold mb-12">AI BASED CAREER MENTOR</h1>
-//                     <h2 className="text-2xl font-bold mb-8">Combined History Snapshot</h2>
-//                     <div>
-//                         {barsData.map((bar, index) => (
-//                             <div key={index} className="mb-4">
-//                                 <div className="flex items-center">
-//                                     <div className="w-24">{bar.label}</div>
-//                                     <div className="bg-gray-200 h-4 flex-grow ml-40">
-//                                         <div
-//                                             className="bg-blue-500 h-full"
-//                                             style={{ width: `${bar.percentage}%` }}
-//                                         ></div>
-//                                     </div>
-//                                     <div className="ml-4">{bar.percentage}%</div>
-//                                 </div>
-//                             </div>
-//                         ))}
-//                     </div>
-//                 </div>
-//             </div> */}
-//         </div>
-
-//     );
-// };
-
-
-// export { History };
